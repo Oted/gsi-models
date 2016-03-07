21fec72fef82719ac83aef169cd8d28c0f2e22a0
Oted <herr.olle@gmail.com>
2016-03-07 00:19:07 +0000

    add lowercase field

--- models/item.js
+++ models/item.js
@@ -5,23 +5,24 @@ module.exports = function(Mongoose) {
     var that = this;
 
     var itemSchema = new Mongoose.Schema({
-        _hash       : { type : String, unique : true },
-        _sort       : { type : String, required : true },
-        title       : { type : String },
-        source      : { type : String },
-        type        : { type: String, enum: that.getItemTypes()},
-        data        : { type : Mongoose.Schema.Types.Mixed, required : 'Data is required.' },
-        score       : { type : Number, default : 0 },
-        likes       : { type : Number, default : 0 },
-        dislikes    : { type : Number, default : 0 },
-        ip          : { type : String, default : null },
-        token       : { type : String, default : null },
-        source_type : { type : String, default : 'none' },
-        sfw         : { type : Boolean, default : true },
-        scraped     : { type : Boolean, default : false },
-        enabled     : { type : Boolean, default : true },
-        view_time   : { type : Number, default : 0 },
-        fragments   : { type : Array, default : [] }
+        _hash               : { type : String, unique : true },
+        _sort               : { type : String, required : true },
+        title               : { type : String },
+        lowercase_title     : { type: String, lowercase: true, trim: true },
+        source              : { type : String },
+        type                : { type: String, enum: that.getItemTypes()},
+        data                : { type : Mongoose.Schema.Types.Mixed, required : 'Data is required.' },
+        score               : { type : Number, default : 0 },
+        likes               : { type : Number, default : 0 },
+        dislikes            : { type : Number, default : 0 },
+        ip                  : { type : String, default : null },
+        token               : { type : String, default : null },
+        source_type         : { type : String, default : 'none' },
+        sfw                 : { type : Boolean, default : true },
+        scraped             : { type : Boolean, default : false },
+        enabled             : { type : Boolean, default : true },
+        views               : { type : Number, default : 0 },
+        fragments           : { type : Array, default : [] }
     }).plugin(require('mongoose-times'));
 
     return Mongoose.model('Item', itemSchema);
