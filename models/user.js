8279c447a2006c6c62ace84d907ffa34aba09163
Oted <herr.olle@gmail.com>
2016-03-13 13:24:34 +0000

    add u_agent

--- models/user.js
+++ models/user.js
@@ -3,13 +3,14 @@
  */
 module.exports = function(Mongoose) {
     var itemSchema = new Mongoose.Schema({
-        _token  : { type : String, required : true, unique : true },
-        email   : { type : String, default : null },
-        ip      : { type : String, required : true },
-        fetches : { type : Number, default : 0 },
-        visits  : { type : Number, default : 1 },
-        queries : { type : Array, default : [] },
-        name    : { type : String, default : 'user' }
+        _token      : { type : String, required : true, unique : true },
+        email       : { type : String, default : null },
+        ip          : { type : String, required : true },
+        fetches     : { type : Number, default : 0 },
+        visits      : { type : Number, default : 1 },
+        user_agent  : { type : String, default : 'none'},
+        queries     : { type : Array, default : [] },
+        name        : { type : String, default : 'user' }
     });
 
     return Mongoose.model('User', itemSchema);
